77bcfbe5c8446787c7fae9d6bc7cf59139100e91
cppupu <cppupu@126.com>
2026-03-03 08:22:11 +0800

    发布 v1.3.0 - Rust 后端首个版本

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,8 @@
 pub const MAX_MESSAGES: i64 = 1024;
 pub const PAGE_SIZE: i64 = 20;
 pub const MAX_PAGES: i64 = 51;
-pub const VERSION: &str = "0.2.0";
-pub const VERSION_DATE: &str = "2026-03-02";
+pub const VERSION: &str = "1.3.0";
+pub const VERSION_DATE: &str = "2026-03-03";
 
 // 输入长度限制（按Unicode字符计数）
 pub const MAX_MESSAGE_LENGTH: usize = 10_000;
