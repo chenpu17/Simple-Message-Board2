370cd34424b9b77bb97859c1f67ecc4eeb38e471
cppupu <cppupu@126.com>
2026-03-03 17:05:58 +0800

    Bump version to 1.3.4

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ pub const MAX_MESSAGES: i64 = 1024;
 pub const PAGE_SIZE: i64 = 20;
 pub const MAX_PAGES: i64 = 51;
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-pub const VERSION_DATE: &str = "2026-03-03";
+pub const VERSION_DATE: &str = "2026-03-04";
 
 // 输入长度限制（按Unicode字符计数）
 pub const MAX_MESSAGE_LENGTH: usize = 10_000;
